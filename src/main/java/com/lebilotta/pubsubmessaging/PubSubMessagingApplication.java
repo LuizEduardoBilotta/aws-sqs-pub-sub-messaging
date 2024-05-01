bbd2a888f91c1d60a54e634a733548b4eaceae50
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2024-05-01 16:17:31 -0300

    chore: configuration files

---
+++ src/main/java/com/lebilotta/pubsubmessaging/PubSubMessagingApplication.java
@@ -0,0 +1,12 @@
+package com.lebilotta.pubsubmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubMessagingApplication.class, args);
+	}
+}
